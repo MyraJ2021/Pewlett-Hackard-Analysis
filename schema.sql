cec7e4ac7227c59492762a6fdb1484c84fdfe80b
Myra Jacobs <myrarjacobs@yahoo.com>
2022-02-19 09:26:04 -0500

    Added all tables to schema.

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	  emp_no INT NOT NULL,
+	   emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -17,12 +17,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -35,7 +35,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
--- No Skill Drill
+CREATE TABLE dept_employees (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(20) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
 
 SELECT * FROM departments;
-
