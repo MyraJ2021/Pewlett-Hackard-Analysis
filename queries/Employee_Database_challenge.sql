45b21f31e3846fa5346ec9e9647adf112a3810d8
Myra Jacobs <myrarjacobs@yahoo.com>
2022-02-20 17:49:53 -0500

    Added mentorship_eligibility file, images, and updated spq code.

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1:
 -- Create retirement_titles table
 SELECT emp.emp_no,
 	emp.first_name,
@@ -12,17 +13,34 @@ ON (emp.emp_no = ti.emp_no)
 WHERE (emp.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- Use Dictinct with ORDER BY to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
 INTO unique_titles
 FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, from_date DESC;
 
--- Retrieve count of employees retiring
+-- Retrieve count of employees retiring by title
 SELECT COUNT (title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY (COUNT(title)) DESC;
+
+-- Deliverable 2: Create mentorship eligibility file
+SELECT DISTINCT ON (emp.emp_no) emp.emp_no,
+emp.first_name,
+emp.last_name,
+emp.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as emp
+INNER JOIN dept_employees as de
+ON (emp.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (emp.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01' AND (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp.emp_no;
 
